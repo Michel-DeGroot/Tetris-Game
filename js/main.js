6198fc0cdc4b0a1a9ad193ffc19e938a89a55525
Michel de Groot <contact@micheldegroot.com>
2022-10-18 15:31:20 +0200

    youtube webstorm new commit github

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const scoreDisplay = document.querySelector('#score')
   const startBtn = document.querySelector('#start-button')
   const width = 10
+  let nextRandom = 0
+  let timerID
+  let score = 0
+  const colors = [
+    'orange',
+    'red',
+    'purple',
+    'green',
+    'blue'
+  ]
+
 
   //The Tetrominoes
   const lTetromino = [
@@ -71,20 +82,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // Make  the tetromino move down every second
   timerId = setInterval(moveDown, 1000)
 
-  //assign function to keycodes
+
+  //assign functions to keyCodes
   function control(e) {
-    if (e.keyCode === 37) {
+    if(e.keyCode === 37) {
       moveLeft()
     } else if (e.keyCode === 38) {
-     // rotate
+      rotate()
     } else if (e.keyCode === 39) {
       moveRight()
     } else if (e.keyCode === 40) {
       moveDown()
     }
-
   }
-
   document.addEventListener('keyup', control)
 
   //move down function
@@ -94,13 +104,13 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
     freeze()
   }
-console.log()
 
   //freeze function
   function freeze() {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
       //start a new tetromino falling
+      random = nextRandom
       nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
       currentPosition = 4
@@ -135,6 +145,33 @@ console.log()
     draw()
   }
 
+  //rotate the tetromino
+  function rotate() {
+    undraw()
+    currentRotation ++
+    if(currentRotation === current.length) { //if the current rotation gets to 4, make it go back to 0
+      currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    checkRotatedPosition()
+    draw()
+  }
+
+  ////
+
+  //show up-next tetromino in mini-grid display
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  const displayIndex = 0
+
+  //the Tetrominos without rotations
+  const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+    [0, 1, displayWidth, displayWidth+1], //oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+  ]
 
 
 })
